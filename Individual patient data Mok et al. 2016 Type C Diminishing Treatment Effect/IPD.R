6632b10682c07f14a7f52ebbe287a339580abda9
Martin Hewing <hewingmartin@gmail.com>
2020-06-07 22:24:58 +0100

    Updated files

---
+++ Individual patient data Mok et al. 2016 Type C Diminishing Treatment Effect/IPD.R
@@ -0,0 +1,107 @@
+###################
+#
+# Read program-1.R and program-2.R using the source function
+#
+################### 
+
+source("program-1.R")
+source("program-2.R")
+
+
+#############
+#
+# First, digitize each line in Figure 2C of Mok et al (2016) 
+# to obtain the (x,y) coordinates for each line that correspond to times and 
+# survival probabilities. 
+# 
+# For each line, this will result in a matrix with 2 columns. 
+# Column 1 is time and Column 2 is survival probability for that time. 
+# This will be given for various time points. 
+#
+# This will result in a total of 2 such data files, one file per line. 
+#
+# The R object digitized.file.names, given below, contains the names of these files. 
+#
+##############
+
+
+digitized.file.names = c("mok.a.caroplatin.tsv", "mokg.tsv")
+
+
+################################
+#
+# Now, look below Figures 2C of Mok et al and write down the 
+# number at risk data given for various time points for each line. 
+#
+# The list numbers.below.figure, shown below, contains these data. 
+#
+################################
+
+numbers.below.figure = list(  
+  mok.a.caroplatin = c(608, 412, 118, 22, 3,1,0),
+  mok.a.gefitinib = c(609, 363, 212, 76, 24, 5, 0)
+  
+)
+
+
+####################
+#
+# Now, specify how far along on the x-axis of each line we want to go to extract data. 
+# For example, in one line we may want to go up to time 15 units, 
+# in another line up to time 18 units etc. 
+# 
+# As above, organize these times (integer values) for each line in the same order 
+# as the sheets in the excel file. 
+#
+####################
+
+time = list( time.mok.a.caroplatin = 0:23, 
+             time.mok.a.gefitinib = 0:23
+)
+
+
+
+################
+#
+# arm indicator
+#
+# 1 = mok.c.caroplatin 
+# 2 = mok.c.gefitinib
+#
+################
+
+
+##############
+#
+# The R functions preprocess.digitized.data (Program 1) 
+# and Guyot.individual.data (Program 2) are given below. 
+# Read them into R first. Then execute the commands below to get individual.data.
+#
+##############
+
+individual.data = NULL
+for(ifile in 1:length(digitized.file.names)){
+  digitized.line = read.table(digitized.file.names[ifile], header=T)
+  processed.line.data = preprocess.digitized.data(digitized.line, 
+                                                  numbers.below.figure[[ifile]], 
+                                                  time[[ifile]])
+  individual.line.data = Guyot.individual.data(processed.line.data$condensed.data.set, 
+                                               processed.line.data$nrisk.data, 
+                                               input.arm.id=ifile)
+  individual.data = rbind(individual.data, individual.line.data)
+}
+
+treatment.type = c(
+  rep("Carboplatin plus paclitaxel", length(which(individual.data[,"tmt.arm.number"] == 1))),
+  rep("Gefitinib", length(which(individual.data[,"tmt.arm.number"] == 2))))                   
+
+
+individual.data = as.data.frame(individual.data)
+individual.data$treatment.type = treatment.type
+
+
+############
+#
+# The R object "individual.data" contains the digitized data set
+#
+############
---
+++ Individual patient data Van Cutsem et al. 2018 Type A Proportional Hazards/IPD.R
@@ -0,0 +1,88 @@
+###################
+#
+# Read program-1.R and program-2.R using the source function
+#
+################### 
+
+source("program-1.R")
+source("program-2.R")
+
+# The R object digitized.file.names, given below, contains the names of these files. 
+#
+##############
+
+
+digitized.file.names = c("van-cutsem-FTD.tsv", "van-cutsem-Placebo.tsv")
+
+
+################################
+#
+# The list numbers.below.figure, shown below, contains numbers at risk data. 
+#
+################################
+
+numbers.below.figure = list(  
+  van.cutsem.FTD = c(534, 521, 499, 459, 406, 355, 308, 267, 
+                         231, 212, 180, 156, 137, 117, 95, 74, 59, 49, 38, 29, 20, 17, 14, 12, 10, 7, 4, 1),    
+  van.cutsem.Placebo = c(266, 259, 232, 198, 163, 137, 114, 
+                          94, 71, 62, 56, 51, 43, 36, 27, 21, 16, 15, 14, 10, 8, 7, 6, 6, 4, 3, 1, 1)
+  
+)
+
+
+####################
+#
+# Now, specify how far along on the x-axis of each line we want to go to extract data. 
+#
+####################
+
+time = list( time.van.cutsem.FTD = 0:28, 
+             time.van.cutsem.Placebo = 0:28
+)
+
+
+
+################
+#
+# arm indicator
+#
+# 1 = van-cutsem.FTD
+# 2 = van-cutsem.Placebo
+#
+################
+
+
+##############
+#
+# The R functions preprocess.digitized.data (Program 1) 
+# and Guyot.individual.data (Program 2) are given below. 
+# Read them into R first. Then execute the commands below to get individual.data.
+#
+##############
+
+individual.data = NULL
+for(ifile in 1:length(digitized.file.names)){
+  digitized.line = read.table(digitized.file.names[ifile], header=T)
+  processed.line.data = preprocess.digitized.data(digitized.line, 
+                                                  numbers.below.figure[[ifile]], 
+                                                  time[[ifile]])
+  individual.line.data = Guyot.individual.data(processed.line.data$condensed.data.set, 
+                                               processed.line.data$nrisk.data, 
+                                               input.arm.id=ifile)
+  individual.data = rbind(individual.data, individual.line.data)
+}
+
+treatment.type = c(
+  rep("FTD plus tipiracil", length(which(individual.data[,"tmt.arm.number"] == 1))),
+  rep("Placebo", length(which(individual.data[,"tmt.arm.number"] == 2))))                   
+
+
+individual.data = as.data.frame(individual.data)
+individual.data$treatment.type = treatment.type
+
+
+############
+#
+# The R object "individual.data" contains the digitized data set
+#
+############
